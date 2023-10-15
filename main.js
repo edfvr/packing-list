5533af8ebcc97e4f9a3f81aff26d0d150420a091
Ed <edsagoefvr@gmail.com>
2023-10-15 00:17:27 +0000

    Adding Firebase

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-//Challenge: Make it so that when you click the 'Add' button, whatever is written in the input field should be console logged.
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+const appSettings = {
+    databaseURL: "https://packing-list-e2ffb-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEL = document.getElementById("add-btn")
